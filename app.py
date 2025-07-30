ab10a489f6ac5954050927f91b535a83220ee2af
Massa720 <college677@gmail.com>
2025-07-30 17:26:34 +0900

    Inline scraper code into app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 import streamlit as st
-import sys
-import os
-sys.path.append(os.path.dirname(__file__))
-from scraper import get_news_data  # ←ここでエラー解決！
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
+def get_news_data():
+    url = "https://news.yahoo.co.jp/pickup"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    data = []
+    for a in soup.select("li a"):
+        title = a.get_text(strip=True)
+        link = a.get("href")
+        data.append({"タイトル": title, "リンク": link})
+
+    df = pd.DataFrame(data)
+    return df
+
+# Streamlit表示部分
 st.title("Yahooニュース ピックアップ")
-
 df = get_news_data()
 st.dataframe(df)
+
